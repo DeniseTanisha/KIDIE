34e5f9e3b661bd6b5c1f3a930ef1d29f49e1c79f
Denise Stubbs <denise.stubbs@Nology.local>
2019-08-27 16:14:40 +0100

    api fetch working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: []
+  };
+  componentDidMount = () => {
+    this.getEvents();
+  };
+  getEvents() {
+    let that = this;
+    function start() {
+      window.gapi.client
+        .init({
+          apiKey: "AIzaSyBfkp9QX42_w-xbobYmvB1D0vNoFJiDu_E"
+        })
+        .then(function() {
+          return window.gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/nology.io_5smheaincm2skd1tcmvv7m37d8@group.calendar.google.com/events`
+          });
+        })
+        .then(
+          response => {
+            let events = response.result.items;
+            that.setState(
+              {
+                events
+              },
+              () => {
+                console.log(that.state.events);
+              }
+            );
+          },
+          function(reason) {
+            console.log(reason);
+          }
+        );
+    }
+    window.gapi.load("client", start);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.events.map(event => (
+            <li> {event.summary} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// API KEY: AIzaSyBfkp9QX42_w-xbobYmvB1D0vNoFJiDu_E
+// Calendar ID: nology.io_5smheaincm2skd1tcmvv7m37d8@group.calendar.google.com
